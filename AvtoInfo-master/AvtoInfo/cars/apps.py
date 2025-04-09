e057b11184b4a1551bcf2345d1d7293ea50a66b7
Hotecka <125291757+Hotecka@users.noreply.github.com>
2025-04-10 09:59:07 +1000

    apps.py

--- AvtoInfo-master/AvtoInfo/cars/apps.py
+++ AvtoInfo-master/AvtoInfo/cars/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class CarsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "cars"
+    """
+    Конфигурация приложения для работы с автомобилями.
+    Здесь можно задать настройки для приложения 'cars'.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"  # Устанавливаем тип для автоматического поля идентификатора (ID) - BigAutoField
+    name = "cars"  # Имя приложения, используется для настройки в проекте Django
+
