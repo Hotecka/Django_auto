a2ed7aedf206557fe54531098b881868d777f52c
Hotecka <125291757+Hotecka@users.noreply.github.com>
2025-04-10 09:49:01 +1000

    admin.py

--- AvtoInfo-master/AvtoInfo/cars/admin.py
+++ AvtoInfo-master/AvtoInfo/cars/admin.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from cars.models import Car, Comment
 
 
+# Регистрация модели Car в админ-панели
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     pass
 
 
+# Регистрация модели Comment в админ-панели
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     pass
