1f37e8b51c90aeb97276a1149ddf8cb634e27885
Hotecka <125291757+Hotecka@users.noreply.github.com>
2025-04-10 10:15:20 +1000

    forms.py

--- AvtoInfo-master/AvtoInfo/users/forms.py
+++ AvtoInfo-master/AvtoInfo/users/forms.py
@@ -1,44 +1,43 @@
 from django import forms
 from django.contrib.auth.forms import (
-    AuthenticationForm,
-    UserCreationForm,
-    UserChangeForm,
+    AuthenticationForm,  # Форма для аутентификации пользователя
+    UserCreationForm,    # Форма для создания нового пользователя
+    UserChangeForm,      # Форма для изменения данных пользователя
 )
 
-from users.models import User
+from users.models import User  
 
-
+# Форма для аутентификации пользователя
 class UserLoginForm(AuthenticationForm):
-
     class Meta:
-        model = User
+        model = User  # Модель пользователя, с которой связана форма
         fields = [
-            "username",
-            "password",
+            "username",  # Поле для ввода логина
+            "password",  # Поле для ввода пароля
         ]
 
-
+# Форма для регистрации нового пользователя
 class UserRegistrationForm(UserCreationForm):
-
     class Meta:
-        model = User
+        model = User  # Модель пользователя для регистрации
         fields = [
-            "username",
-            "email",
-            "password1",
-            "password2",
+            "username",  # Поле для ввода логина
+            "email",     # Поле для ввода email
+            "password1", # Поле для ввода пароля
+            "password2", # Поле для подтверждения пароля
         ]
 
-    email = forms.CharField()
+    
+    email = forms.CharField() 
 
-
+# Форма для изменения профиля пользователя
 class UserProfileForm(UserChangeForm):
-
     class Meta:
-        model = User
+        model = User  # Модель пользователя для изменения профиля
         fields = [
-            "username",
-            "email",
+            "username",  # Поле для ввода логина
+            "email",     # Поле для ввода или изменения email
         ]
 
-    email = forms.CharField()
+    
+    email = forms.CharField() 
