33f12be5117f3f6ed62db9e8d726f4edcf625e2e
Hotecka <125291757+Hotecka@users.noreply.github.com>
2025-04-10 10:13:00 +1000

    urls.py

--- AvtoInfo-master/AvtoInfo/users/urls.py
+++ AvtoInfo-master/AvtoInfo/users/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-
 from users import views
 
 app_name = "users"
 
-
 urlpatterns = [
-    path("login/", views.login, name="login"),
-    path("registration/", views.registration, name="registration"),
-    path("profile/", views.profile, name="profile"),
-    path("logout/", views.logout, name="logout"),
+    path("login/", views.login, name="login"),  # Страница входа
+    path("registration/", views.registration, name="registration"),  # Страница регистрации
+    path("profile/", views.profile, name="profile"),  # Страница профиля пользователя
+    path("logout/", views.logout, name="logout"),  # Страница выхода
 ]
